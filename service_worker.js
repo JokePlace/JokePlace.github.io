4ccf9064919d00c3139e65238e1f377f9d5c590d
JokePlace <50629096+JokePlace@users.noreply.github.com>
2019-05-15 00:04:48 +0900

    new

---
+++ service_worker.js
@@ -0,0 +1,29 @@
+//キャッシュファイルの指定
+var CACHE_NAME = 'pwa-sample-caches';
+var urlsToCache = ['/192.168.3.179:5500/',];
+
+//インストール処理
+self.addEventListener('insrall',function(event)
+{
+    event.waitUntil(
+        caches
+            .open(CACHE_NAME)
+            .then(function(cache)
+            {
+                return cache.addAll(urlsToCache);
+            })
+    );
+});
+
+//リソースフェッチ時のキャッシュロード処理
+self.addEventListener('fetch',function(event)
+{
+    event.respondWith(
+        caches
+            .match(event.request)
+            .then(function(responce)
+            {
+                return responce ? responce :fetch(event.request);
+            })
+    );
+});
